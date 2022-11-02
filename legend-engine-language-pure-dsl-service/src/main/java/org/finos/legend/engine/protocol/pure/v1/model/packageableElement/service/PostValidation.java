f4126e3bc93c95e119ca7ef2e6af84e7a4e38254
Tom Wilson <thomas.wilson@gs.com>
2022-11-02 12:10:51 +0000

    merge changes from dihuibao-gs/legend-engine:service_post_validation

--- legend-engine-language-pure-dsl-service/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/service/PostValidation.java
+++ legend-engine-language-pure-dsl-service/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/service/PostValidation.java
@@ -1,20 +1,23 @@
-//  Copyright 2022 Goldman Sachs
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+/*
+ *  Copyright 2022 Goldman Sachs
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 package org.finos.legend.engine.protocol.pure.v1.model.packageableElement.service;
 
-import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.ValueSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.SourceInformation;
+import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.Lambda;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +25,7 @@ import java.util.List;
 public class PostValidation
 {
     public String description;
-    public List<ValueSpecification> parameters = Collections.emptyList();
+    public List<Lambda> parameters = Collections.emptyList();
     public List<PostValidationAssertion> assertions = Collections.emptyList();
+    public SourceInformation sourceInformation;
 }
